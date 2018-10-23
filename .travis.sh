4f7ae211f19b1b97b53f5d788a999ded2d95bf62
Pim Otte <potte@quintor.nl>
2018-10-23 15:35:48 +0200

    Moving compile further down

--- .travis.sh
+++ .travis.sh
@@ -9,13 +9,15 @@ echo "LOCAL IP: $TEST_POOL_IP"
 
 echo "ENDPOINT_IP=\"$TEST_POOL_IP\"" > gradle.properties
 
-./gradlew assemble
 
 cd StudyBits
 docker build -t quindy:latest quindy/
+docker run -v $HOME/.m2/:/root/m2 quindy:latest sh -c "mvn install -DskipTests && cp -r /root/.m2/repository /root/m2"
+
+./gradlew assemble
+
 docker-compose up -d --build --force-recreate pool university-agent-rug university-agent-gent
 
-docker run -v $HOME/.m2/:/root/m2 quindy:latest sh -c "mvn install -DskipTests && cp -r /root/.m2/repository /root/m2"
 
 echo "Watining for dockers to start"
 while [ -n "$(docker ps -a | grep starting)" ]; 
