c05487940781ec945f112e67e5dd74f619fe8a77
Pim Otte <potte@quintor.nl>
2018-10-23 10:57:26 +0200

    Small fixes, trying to get CI to work

--- download-deps.sh
+++ download-deps.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 LIBINDY_VERSION=1.6.6
-
-wget -O build/tmp.zip "https://repo.sovrin.org/android/libindy/stable/$LIBINDY_VERSION/libindy_android_x86_$LIBINDY_VERSION.zip" 
-unzip build/tmp.zip -d build/ 
+ZIPFILE="libindy_android_x_86_$LIBINDY_VERSION.zip"
+if [ ! -f "build/libindy_x86" ]; then
+    echo "Downloading zipfile"
+    wget -O "build/$ZIPFILE" "https://repo.sovrin.org/android/libindy/stable/$LIBINDY_VERSION/$ZIPFILE" 
+    unzip build/$ZIPFILE -d build/ 
+fi
 cp $(find build/libindy_x86 -name libindy.so) app/src/main/jniLibs/x86 
