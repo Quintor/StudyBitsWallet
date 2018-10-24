bdaa114226758fc0708d8f7d4ade8f8e10252ad0
Pim Otte <potte@quintor.nl>
2018-10-24 15:26:16 +0200

    Checking memory usage

--- ci/travis.sh
+++ ci/travis.sh
@@ -20,7 +20,9 @@ adb shell input keyevent 82 &
 echo "Waiting for dockers to start"
 while [ -n "$(docker ps -a | grep starting)" ]; 
 do
-    sleep 1
+    free -m
+    echo $(docker ps -a | grep starting)
+    sleep 5
 done 
 
 echo "Dockers are booted"
