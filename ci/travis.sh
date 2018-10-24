bb7a0a697a82f1320ddf9eb06270e59c9b18b0fa
Pim Otte <potte@quintor.nl>
2018-10-24 12:25:22 +0200

    Fixing environment issues

--- ci/travis.sh
+++ ci/travis.sh
@@ -13,9 +13,8 @@ cd ..
 ./gradlew assemble
 cd StudyBits
 docker-compose up -d --build --force-recreate pool university-agent-rug university-agent-gent
-if [ -n "$IN_TRAVIS" ]; then
-    android-wait-for-emulator
-fi
+
+android-wait-for-emulator
 adb shell settings put global window_animation_scale 0 &
 adb shell settings put global transition_animation_scale 0 &
 adb shell settings put global animator_duration_scale 0 &
