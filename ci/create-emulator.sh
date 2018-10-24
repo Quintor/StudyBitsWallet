a0e678761ae1bf484234e2f02544510ee25fbcba
Pim Otte <potte@quintor.nl>
2018-10-24 15:18:08 +0200

    Correct script

--- ci/create-emulator.sh
+++ ci/create-emulator.sh
@@ -2,6 +2,6 @@
 set -e
 echo no | android create avd --force -n test -t android-24 --abi google_apis/armeabi-v7a
 emulator -avd test  -no-window &
-android_wait_for_emulator
+android-wait-for-emulator
 
 
