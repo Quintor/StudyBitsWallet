c2f4a7728de0cb6c40d8d460744977164f6b42a9
Pim Otte <potte@quintor.nl>
2018-10-24 14:24:31 +0200

    Correcting target abi

--- ci/create-emulator.sh
+++ ci/create-emulator.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-echo no | android create avd --force -n test -t google_apis-24 --abi armeabi-v7a
+echo no | android create avd --force -n test -t android-24 --abi google_apis/armeabi-v7a
 emulator -avd test  -no-window &
 
 
