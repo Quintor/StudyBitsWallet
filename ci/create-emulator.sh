45a3b7701cd93cf1c3587b9a9a5121dafe0468ec
Pim Otte <potte@quintor.nl>
2018-10-26 13:22:55 +0200

    Experimenting with emulator options

--- ci/create-emulator.sh
+++ ci/create-emulator.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 echo no | android create avd --force -n test -t android-24 --abi google_apis/armeabi-v7a
-emulator -avd test  -no-window &
+emulator -avd test -netdelay none -netspeed full   -no-window &
 #android-wait-for-emulator
 
 
