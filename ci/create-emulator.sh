47980369960ae55f56a9c468480c9f3848563a1d
Pim Otte <potte@quintor.nl>
2018-10-30 14:42:53 +0100

    Remove no-audio and debug something

--- ci/create-emulator.sh
+++ ci/create-emulator.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 echo no | android create avd --force -n test -t android-24 --abi x86
-emulator -avd test -netdelay none -netspeed full  -no-audio -no-window -no-accel &
+emulator -avd test -netdelay none -netspeed full  -no-window -no-accel &
+sleep 30
+adb shell getprop
 android-wait-for-emulator
 
