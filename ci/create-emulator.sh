da5a1a59684a88b2301556bfb9d42366d6a17e5f
Pim Otte <potte@quintor.nl>
2018-10-30 14:24:39 +0100

    Test switching to x86 emulator

--- ci/create-emulator.sh
+++ ci/create-emulator.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
-echo no | android create avd --force -n test -t android-24 --abi google_apis/armeabi-v7a
-emulator -avd test -netdelay none -netspeed full   -no-window &
-#android-wait-for-emulator
-
+echo no | android create avd --force -n test -t android-24 --abi x86 -no-accel
+emulator -avd test -netdelay none -netspeed full  -no-audio -no-window &
+android-wait-for-emulator
 
