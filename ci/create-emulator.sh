c2539c79020467097fc6c8d760658f533248a29a
Pim Otte <potte@quintor.nl>
2018-10-30 14:54:15 +0100

    Testing through tcp/ip?

--- ci/create-emulator.sh
+++ ci/create-emulator.sh
@@ -3,6 +3,6 @@ set -e
 echo no | android create avd --force -n test -t android-24 --abi x86
 emulator -avd test -netdelay none -netspeed full  -no-window -no-accel &
 sleep 30
-adb shell getprop
+adb -e shell getprop
 android-wait-for-emulator
 
