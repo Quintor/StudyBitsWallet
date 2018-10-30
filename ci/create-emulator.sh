10d41e5642f82ae7e42cb9117a117d551533726d
Pim Otte <potte@quintor.nl>
2018-10-30 14:35:05 +0100

    Moving no-accel option to correct place, maybe?

--- ci/create-emulator.sh
+++ ci/create-emulator.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-echo no | android create avd --force -n test -t android-24 --abi x86 -no-accel
-emulator -avd test -netdelay none -netspeed full  -no-audio -no-window &
+echo no | android create avd --force -n test -t android-24 --abi x86
+emulator -avd test -netdelay none -netspeed full  -no-audio -no-window -no-accel &
 android-wait-for-emulator
 
