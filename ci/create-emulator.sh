95ed54e50e43bfd352b892783894785f6da29b87
Pim Otte <potte@quintor.nl>
2018-10-31 12:57:59 +0100

    Reinstate no-audio, add no-skin

--- ci/create-emulator.sh
+++ ci/create-emulator.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 echo no | android create avd --force -n test -t android-24 --abi x86
-emulator -avd test -netdelay none -netspeed full  -no-window -no-accel &
+emulator -avd test -netdelay none -netspeed full -no-window -no-audio -no-skin -no-accel &
 sleep 30
 adb -e shell getprop
 android-wait-for-emulator
