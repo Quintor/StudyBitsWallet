56c7faabfdd3c67e0f078cc9f100f70bbfa6d791
Pim Otte <potte@quintor.nl>
2018-11-01 10:19:12 +0100

    Checking without -no-audio option

--- ci/create-emulator.sh
+++ ci/create-emulator.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 echo no | android create avd --force -n test -t android-24 --abi armeabi-v7a
-emulator -avd test -netdelay none -netspeed full -no-window -no-audio &
+emulator -avd test -netdelay none -netspeed full -no-window &
 #android-wait-for-emulator
+sleep 5
