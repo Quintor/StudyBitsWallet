47bab3de7a800e08d86dc9146e87e5903e675091
Pim Otte <potte@quintor.nl>
2018-11-01 10:10:50 +0100

    Not waiting for emulator to parallellize boot

--- ci/create-emulator.sh
+++ ci/create-emulator.sh
@@ -2,4 +2,4 @@
 set -e
 echo no | android create avd --force -n test -t android-24 --abi armeabi-v7a
 emulator -avd test -netdelay none -netspeed full -no-window -no-audio &
-android-wait-for-emulator
+#android-wait-for-emulator
